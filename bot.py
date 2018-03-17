9cc8b8ad56e27fc2f58456b85ed7f91912287c49
Artem Razumov <airplaneless@github.com>
2018-03-17 20:11:00 +0500

    init

--- bot.py
+++ bot.py
@@ -4,22 +4,35 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 from src.interface import Interface
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, DispatcherHandlerStop
+
+
+def check_allowed(bot, update):
+    if update.message.from_user.id not in [343257579]:
+        raise DispatcherHandlerStop
+
 
 if __name__ == '__main__':
 
     with open('token', mode='r') as f:
         __token__ = f.readline()
 
+    with open('users', mode='r') as f:
+        users = f.readlines()
+        users = map(lambda s: int(s), users)
+
+
+
     updater = Updater(__token__)
     dispatcher = updater.dispatcher
 
-    interface = Interface()
+    interface = Interface(users)
 
     start_handler = CommandHandler('start', interface.start)
-    check_handler = CommandHandler('squeue-me', interface.check_queue)
-    checkall_handler = CommandHandler('squeue-all', interface.check_queue)
+    check_handler = CommandHandler('squeue_me', interface.check_queue)
+    checkall_handler = CommandHandler('squeue_all', interface.check_queue)
 
+    #dispatcher.add_handler(MessageHandler(Filters.all, check_allowed))
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(check_handler)
     dispatcher.add_handler(checkall_handler)
--- src/interface.py
+++ src/interface.py
@@ -3,19 +3,21 @@
 import logging
 import datetime
 import os
+from telegram.ext import DispatcherHandlerStop
 
 
 def usr2str(usr):
-    return "{} {} ({})".format(
+    return "{} {} ({}, {})".format(
         usr['first_name'],
         usr['last_name'],
-        usr['username']
+        usr['username'],
+        usr['id']
     )
 
 
 class Interface:
 
-    def __init__(self):
+    def __init__(self, users):
         time = datetime.datetime.now()
         log_file = os.path.abspath('bot_logs/bot_{0}_{1}_{2}.log'.format(time.year, time.month, time.day))
         if not os.path.exists(os.path.abspath('bot_logs/')):
@@ -29,27 +31,30 @@ class Interface:
             format='%(asctime)s %(message)s'
         )
 
-    @staticmethod
-    def start(bot, update, args):
+        self.users = users
+
+    def start(self, bot, update):
         user = update.message.from_user
-        for arg in args:
-            print arg
+        if update.message.from_user.id not in self.users:
+            raise DispatcherHandlerStop
         logging.info('Hi to {}'.format(usr2str(user)))
         bot.send_message(chat_id=update.message.chat_id, text=u"Hello there")
 
-    @staticmethod
-    def check_queue(bot, update):
+    def check_queue(self, bot, update):
         user = update.message.from_user
         logging.info('check queue for {}'.format(usr2str(user)))
+        if update.message.from_user.id not in self.users:
+            raise DispatcherHandlerStop
         os.system('squeue -u u0052 > workspace/queue.txt')
         with open('queue.txt', mode='r') as f:
             text = f.readlines()
         bot.send_message(chat_id=update.message.chat_id, text='\n'.join(text))
 
-    @staticmethod
-    def check_queue_all(bot, update):
+    def check_queue_all(self, bot, update):
         user = update.message.from_user
         logging.info('check all queue for {}'.format(usr2str(user)))
+        if update.message.from_user.id not in self.users:
+            raise DispatcherHandlerStop
         os.system('squeue > workspace/queue_all.txt')
         with open('queue.txt', mode='r') as f:
             text = f.readlines()
