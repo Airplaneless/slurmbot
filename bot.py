fc49fc1ae0b4d917d4a199285b11ea18acc26757
Artem Razumov <airplaneless@github.com>
2018-03-19 16:35:28 +0500

    moar

--- bot.py
+++ bot.py
@@ -3,31 +3,32 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+import json
 from src.interface import Interface
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, DispatcherHandlerStop
 
 
 if __name__ == '__main__':
 
-    with open('token', mode='r') as f:
-        __token__ = f.readline()
+    with open('config.json', mode='r') as f:
+        config = json.load(f)
 
-    with open('users', mode='r') as f:
-        users = f.readlines()
-        users = map(lambda s: int(s), users)
-
-    updater = Updater(__token__)
+    updater = Updater(config['token'])
     dispatcher = updater.dispatcher
 
-    interface = Interface(users)
+    interface = Interface(config['allowed_users'], config['output_dir'])
 
     start_handler = CommandHandler('start', interface.start)
     check_handler = CommandHandler('squeue_me', interface.check_queue)
-    checkall_handler = CommandHandler('squeue_all', interface.check_queue)
+    checkall_handler = CommandHandler('squeue_all', interface.check_queue_all)
+    check_space_handler = CommandHandler('diskspace', interface.check_disk_space)
+    check_progress_handler = CommandHandler('progress', interface.check_progress)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(check_handler)
     dispatcher.add_handler(checkall_handler)
+    dispatcher.add_handler(check_space_handler)
+    dispatcher.add_handler(check_progress_handler)
 
     updater.start_polling()
     updater.idle()
--- src/interface.py
+++ src/interface.py
@@ -17,7 +17,7 @@ def usr2str(usr):
 
 class Interface:
 
-    def __init__(self, users):
+    def __init__(self, users, progress_dir):
         time = datetime.datetime.now()
         log_file = os.path.abspath('bot_logs/bot_{0}_{1}_{2}.log'.format(time.year, time.month, time.day))
         if not os.path.exists(os.path.abspath('bot_logs/')):
@@ -32,6 +32,7 @@ class Interface:
         )
 
         self.users = users
+        self.progress_dir = os.path.abspath(progress_dir)
 
     def start(self, bot, update):
         user = update.message.from_user
@@ -50,6 +51,16 @@ class Interface:
             text = f.readlines()
         bot.send_message(chat_id=update.message.chat_id, text='\n'.join(text))
 
+    def check_disk_space(self, bot, update):
+        user = update.message.from_user
+        logging.info('check disk space for {}'.format(usr2str(user)))
+        if update.message.from_user.id not in self.users:
+            raise DispatcherHandlerStop
+        os.system('du -sh > workspace/disk_space.txt')
+        with open('workspace/disk_space.txt', mode='r') as f:
+            text = f.readlines()
+        bot.send_message(chat_id=update.message.chat_id, text='\n'.join(text))
+
     def check_queue_all(self, bot, update):
         user = update.message.from_user
         logging.info('check all queue for {}'.format(usr2str(user)))
@@ -59,3 +70,18 @@ class Interface:
         with open('workspace/queue_all.txt', mode='r') as f:
             text = f.readlines()
         bot.send_message(chat_id=update.message.chat_id, text='\n'.join(text))
+
+    def check_progress(self, bot, update):
+        user = update.message.from_user
+        logging.info('check progress for {}'.format(usr2str(user)))
+        if update.message.from_user.id not in self.users:
+            raise DispatcherHandlerStop
+        progress = list()
+        for root, dirs, files in os.walk(os.path.abspath(self.progress_dir)):
+            for f in files:
+                if '.txt' in f:
+                    name = root.split('/')[-1]
+                    with open(os.path.join(root, f), mode='r') as progress_file:
+                        progress.append(name + ':' + progress_file.readlines()[-1])
+
+        bot.send_message(chat_id=update.message.chat_id, text='\n'.join(progress))
